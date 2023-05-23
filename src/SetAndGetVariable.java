ed31411089b69954aeca1431d12ca3a26fcba9c0
ARAJO <dddd2511u@gmail.com>
2023-05-23 18:25:38 +0900

    1234

---
+++ src/SetAndGetVariable.java
@@ -0,0 +1,35 @@
+public class SetAndGetVariable {
+    public static void main(String[] args)  {
+        int A = 1;
+        int B = 3;
+        int C = 5;
+        int D = 7;
+
+    
+        
+        if (B == 3) {
+            A = 10;
+        }
+       
+        if (D !=9 ){
+            B = 5;
+        } else {
+            B = 200;
+        }
+
+        if (A == 10) {
+            C = 50;
+        } else {
+            D = 30;
+        }
+
+        System.out.println(A);
+        System.out.println(B);
+        System.out.println(C);
+        System.out.println(D);
+
+    }   
+    }
+        // return 0;
+
+
