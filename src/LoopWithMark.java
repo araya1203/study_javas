57976c42c2b3b5bed59015b176f1f792f86509c0
ARAJO <dddd2511u@gmail.com>
2023-05-24 15:29:50 +0900

    8

---
+++ src/LoopWithMark.java
@@ -0,0 +1,20 @@
+public class LoopWithMark {
+    public static void main(String[] args) {
+        // for (statement 1; statement 2; statement 3) {
+            // code block to be executed
+        //   }
+         
+        int B = 5;
+          for (int A = 1;  A < B; A = A + 1) {
+            // code block to be executed
+            System.out.println( "for"+""+A);
+     
+        //   }
+          
+        }
+        System.out.println("End");
+       
+    // return 0; 
+    }
+    
+}
