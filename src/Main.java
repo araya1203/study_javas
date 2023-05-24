03be05a32c5ecd4442acd0aa8da6cb2a7a372cdb
ARAJO <dddd2511u@gmail.com>
2023-05-24 13:00:19 +0900

    44

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,11 @@
 public class Main { 
     public static void main(String[] args) {
-         String name = "Ara Jo";
-         System.out.println(name);
-         name = "Jo";
-         System.out.println(name);
-         // 위에  System.out.println(); black point 값을 넣기 위해 입력한 것임. 필요없음.
+        // refer: https://www.acmicpc.net/problem/2557
+        //문제 Hello World!를 출력하시오.
+         System.out.println ("Hello World!를 출력"); 
+
+        
+      }
         // return 0;
     }
 }
