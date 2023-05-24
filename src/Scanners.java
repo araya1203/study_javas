1e604ffbe730e651c91265b8717515a6d7903b25
ARAJO <dddd2511u@gmail.com>
2023-05-24 14:02:36 +0900

    77

--- src/Scanners.java
+++ src/Scanners.java
@@ -1,5 +1,5 @@
 import java. util.Scanner;
-public class Scaners {
+public class Scanners {
 public static void main(String[] args) {
         Scanner myObj = new Scanner(System.in);  // Create a Scanner object
         // System.out.println("Enter username");
