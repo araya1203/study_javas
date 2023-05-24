fc6081917fc25a1359754a258e274f5882d58f37
ARAJO <dddd2511u@gmail.com>
2023-05-24 11:19:06 +0900

    22

--- src/LoopWithCounter.java
+++ src/LoopWithCounter.java
@@ -5,10 +5,13 @@ public class LoopWithCounter {
           int A = 5;
           int B = 1;
           int C = 1;
-          while ( C < A) {
+          while ( C < A && B < A) {
+            System.out.println(B);
             System.out.println(C);
+             B = B + 1 ;
              C = C + 1 ;
           }
+            System.out.println(B);
             System.out.println(C);  
         // reurn 0;
     }
