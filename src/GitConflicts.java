e8fbee138f1081c0d226de1433229bbe7a860b7b
ARAJO <dddd2511u@gmail.com>
2023-05-31 11:42:00 +0900

    GitConflicts.java

---
+++ src/GitConflicts.java
@@ -0,0 +1,16 @@
+public class GitConflicts {
+    public static void main(String[] args) {
+         // try catch문 사용
+        try {
+            int second = 1;
+            int first = 0;
+            second = first + 1
+            
+        } catch (Exception e) {
+            // TODO: handle exception
+        
+        }
+
+       // return 0; 
+    }
+}
