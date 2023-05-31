44e1821c7ffe434e6646c39d8f44f5a18a04d040
Nahyuntak <hyuntak@snu.ac.kr>
2023-05-31 12:18:09 +0900

    first commit

---
+++ src/HyuntakNa.java
@@ -0,0 +1,9 @@
+public class HyuntakNa {
+    public static void main(String[] args) {
+        try {
+            System.out.println("나현탁");
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+    }
+}
