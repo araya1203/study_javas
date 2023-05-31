10d8398cbd91df51bf372047920a016665df57f4
ARAJO <dddd2511u@gmail.com>
2023-05-31 11:55:28 +0900

    GitConflicts.java

--- src/GitConflicts.java
+++ src/GitConflicts.java
@@ -1,2 +1,7 @@
-git add src/GitConflicts.java
-git commit -m "Resolved merge conflict in GitConflicts.java"
+public class GitConflicts {
+    public static void main(String[] args) {
+        int second = 1;
+        int first = 0;
+       second = first + 1;
+    }
+}
