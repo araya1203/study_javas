2fd52f16afe1d4e2b462cf11fe0505a87a00c061
ARAJO <dddd2511u@gmail.com>
2023-06-12 16:37:04 +0900

    first commit

--- src/Cars/FactoryDMLs.java
+++ src/Cars/FactoryDMLs.java
@@ -5,9 +5,13 @@ import java.sql.ResultSet;
 import java.sql.Statement;
 
 public class FactoryDMLs {
-    public ResultSet SelectStatements( Statement statement,String query) throws SQLException {
-    ResultSet resultSet = statement.executeQuery(query);
-    return resultSet;
+    public ResultSet selectStatements(Statement statement, String query) throws SQLException {
+        ResultSet resultSet = statement.executeQuery(query);
+        return resultSet;
+    }
 
+    public int insertStatements(Statement statement, String query) throws SQLException {
+        int count = statement.executeUpdate(query);
+        return count;
     }
 }
--- src/ConnectDBsWithMethod.java
+++ src/ConnectDBsWithMethod.java
@@ -1,9 +1,12 @@
 import java.sql.*;
-
 import Cars.FactoryDMLs;
 
 public class ConnectDBsWithMethod {
+   
     public static void main(String[] args) {
+         Connection connection;
+         Statement statement;
+       
         try {
             // - MySQL workbench 실행 : JDBC
             // - User/password와 접속 IP:port 접속
@@ -11,22 +14,23 @@ public class ConnectDBsWithMethod {
             String user = "root";
             String password = "!yojulab*";
 
-            
-            Connection connection = DriverManager.getConnection(url, user, password);
+
+            connection = DriverManager.getConnection(url, user, password);
             System.out.println("DB연결 성공\n");
 
             // - query Edit
-            Statement statement = connection.createStatement();
+            statement = connection.createStatement();
+            // statement에 계속 query를 던지는 것이다. 
             String query = "SELECT * FROM factorys";
             FactoryDMLs factoryDMLs = new FactoryDMLs();
-            ResultSet resultSet = factoryDMLs.SelectStatements(statement, query);
+            ResultSet resultSet = factoryDMLs.selectStatements(statement, query);
             while (resultSet.next()) {
                 System.out.println(resultSet.getString("COMPANY_ID") + resultSet.getString("COMPANY"));
             }
 
             // SELECT COUNT(*) AS CNT FROM factorys;
             query = "SELECT COUNT(*) AS CNT FROM factorys";
-            resultSet = statement.executeQuery(query);
+            resultSet = factoryDMLs.selectStatements(statement, query);
             int totalCount = 0;
             while (resultSet.next()) {
                 System.out.println(resultSet.getInt("CNT"));
@@ -46,13 +50,21 @@ public class ConnectDBsWithMethod {
                     " VALUE " +
                     "('"+companyId+"', '"+company+"') ";
             
-            int count = statement.executeUpdate(query);
+            int count = factoryDMLs.insertStatements(statement, query);
             System.out.println();
+            statement.close(); 
+             connection.close();
+
         } catch (Exception e) {
             // TODO: handle exception
             System.out.println(e.getMessage());
+        }finally{
+            // TODO: handle exception
         }
         System.out.println();
     }
 }
+
+
+    
 
