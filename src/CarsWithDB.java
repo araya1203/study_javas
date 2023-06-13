b1f4d6bb49590901a36dc4d379585748c1d36b22
ARAJO <dddd2511u@gmail.com>
2023-06-13 12:31:52 +0900

    first commit

--- src/CarsWithDB.java
+++ src/CarsWithDB.java
@@ -9,7 +9,7 @@ public class CarsWithDB {
         try {
             // - MySQL workbench 실행 : JDBC
             // - User/password와 접속 IP:port 접속
-            String url = "jdbc:mysql://127.0.0.1:3306/db_cars";
+            String url = "jdbc:mysql://localhost:3306/db_cars";
             String user = "root";
             String password = "!yojulab*";
 
@@ -23,13 +23,28 @@ public class CarsWithDB {
             // 작업 키 입력
             Scanner scanner = new Scanner(System.in);
             String workkey = "P";
-            while(!workkey.equals("E")) {
+            while (!workkey.equals("E")) {
                 System.out.println("선택입력 : ");
                 workkey = scanner.nextLine();
                 if (workkey.equals("O")) {
-                    System.out.println("차 이름 명단") ;
+                    System.out.println("차 이름 명단");
+                    query = "SELECT T_FAC.COMPANY, T_CAR_INFOR.CAR_NAME\n" + //
+                            ", T_CAR_INFOR.CAR_INFOR_ID\n" + //
+                            "FROM (factorys AS T_FAC\n" + //
+                            "     inner JOIN car_infors AS T_CAR_INFOR\n" + //
+                            "     ON T_FAC.COMPANY_ID = T_CAR_INFOR.COMPANY_ID) ";
+                    ResultSet resultSet = statement.executeQuery(query);
+                    int number = 1;
+                    while (resultSet.next()) {
+                        System.out.println(number + "." +
+                                resultSet.getString("COMPANY") + "-" +
+                                resultSet.getString("CAR_NAME"));
+                        number = number + 1;
+
+                    }
+
                 } else if (workkey.equals("S")) {
-                    System.out.println("통계 시작") ;
+                    System.out.println("통계 시작");
                     query = "SELECT T_FAC.COMPANY, T_CAR_INFOR.CAR_NAME, count(*) AS CNT\n" + //
                             "FROM (factorys AS T_FAC\n" + //
                             "\tinner JOIN car_infors AS T_CAR_INFOR\n" + //
@@ -37,14 +52,15 @@ public class CarsWithDB {
                             "    inner join `options` AS T_OPTS\n" + //
                             "    ON T_CAR_INFOR.CAR_INFOR_ID = T_OPTS.CAR_INFOR_ID\n" + //
                             "    GROUP BY T_FAC.COMPANY_ID, T_CAR_INFOR.CAR_INFOR_ID";
-                    ResultSet resultSet = statement.executeQuery(query) ;
+
+                    ResultSet resultSet = statement.executeQuery(query);
                     while (resultSet.next()) {
                         System.out.println(resultSet.getString("COMPANY") + "," +
-                        resultSet.getString("CAR_NAME") + "," +
-                        resultSet.getInt("CNT"));
+                                resultSet.getString("CAR_NAME") + "," +
+                                resultSet.getInt("CNT"));
                     }
                 } else {
-                    System.out.println("----- 작업 키 확인 : 입력한 값 : "+workkey) ;
+                    System.out.println("----- 작업 키 확인 : 입력한 값 : " + workkey);
                 }
             }
             System.out.println("----- 작업 종료 -----");
@@ -55,5 +71,3 @@ public class CarsWithDB {
         System.out.println();
     }
 }
-    
-
