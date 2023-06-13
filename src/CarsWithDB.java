8687361f6eb872fe5f9c6ab95456f9031a034d65
ARAJO <dddd2511u@gmail.com>
2023-06-13 13:25:57 +0900

    first commit

--- src/CarsWithDB.java
+++ src/CarsWithDB.java
@@ -2,9 +2,8 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.HashMap;
 import java.util.Scanner;
-
-import javax.management.Query;
 
 public class CarsWithDB {
     public static void main(String[] args) {
@@ -24,32 +23,36 @@ public class CarsWithDB {
 
             // 작업 키 입력
             Scanner scanner = new Scanner(System.in);
-            String workkey = "P";
-            while(!workkey.equals("E")) {
+            String workKey = "P";
+            while (!workKey.equals("E")) {
                 System.out.print("선택입력 : ");
-                workkey = scanner.nextLine();
-                if (workkey.equals("O")) {
-                    System.out.println("차 이름 명단") ;
+                workKey = scanner.nextLine();
+                if (workKey.equals("O")) {
+                    // 차 이름 명단
+                    System.out.println("- 차 이름 명단");
                     query = "SELECT T_FAC.COMPANY, T_CAR_INFOR.CAR_NAME\n" + //
                             "\t, T_CAR_INFOR.CAR_INFOR_ID\n" + //
                             "FROM (factorys AS T_FAC\n" + //
                             "\tinner JOIN car_infors AS T_CAR_INFOR\n" + //
-                            "    ON T_FAC.COMPANY_ID = T_CAR_INFOR.COMPANY_ID)\n" ;
-                    ResultSet resultSet = statement.executeQuery(query);
-                    
+                            "    ON T_FAC.COMPANY_ID = T_CAR_INFOR.COMPANY_ID)";
+                    ResultSet resultSet = statement.executeQuery(query) ;
+
                     int number = 1;
                     Statement statement2 = connection.createStatement();
                     ResultSet resultSet2;
                     String query2;
+                    HashMap<String, String> carNumberMap = new HashMap<>();
                     while (resultSet.next()) {
-                        System.out.print(number + "." +
+                        System.out.print(number + ". " +
                             resultSet.getString("COMPANY") + " - " +
                             resultSet.getString("CAR_NAME") + ": ");
+                          String carInforId =   resultSet.getString("CAR_INFOR_ID");
+                          carNumberMap.put(String.valueOf(number),carInforId);
                         query2 = "SELECT T_OPT_INFO.OPTION_NAME\n" + //
                                 "FROM option_infors AS T_OPT_INFO\n" + //
-                                "\tINNER JOIN `options` AS T_OPTS\n" + //
+                                "\tinner join `options` AS T_OPTS\n" + //
                                 "    ON T_OPT_INFO.OPTION_INFOR_ID = T_OPTS.OPTION_INFOR_ID\n" + //
-                                "    AND T_OPTS.CAR_INFOR_ID = '"+resultSet.getString("CAR_INFOR_ID")+"'";
+                                "    AND T_OPTS.CAR_INFOR_ID = '"+carInforId+"'";
                         resultSet2 = statement2.executeQuery(query2);
                         while(resultSet2.next()){
                             System.out.print(resultSet2.getString("OPTION_NAME")+",");
@@ -58,26 +61,33 @@ public class CarsWithDB {
                         System.out.println();
                     }
 
-                } else if (workkey.equals("S")) {
-                    System.out.println("통계 시작") ;
-                    query = "SELECT T_FAC.COMPANY, T_CAR_INFOR.CAR_NAME, count(*) AS CNT\n" + //
+                    // 차량 번호 입력 :
+                    System.out.print("- 차 이름 명단 : ");
+                    String CarNumber = scanner.nextLine();
+                    System.out.println("차명 PK : " + carNumberMap.get(CarNumber));
+
+
+
+                } else if (workKey.equals("S")) {
+                    System.out.println("- 통계 시작 -");
+                    query = "SELECT T_FAC.COMPANY, T_CAR_INFOR.CAR_NAME, COUNT(*) AS CNT\n" + //
                             "FROM (factorys AS T_FAC\n" + //
                             "\tinner JOIN car_infors AS T_CAR_INFOR\n" + //
                             "    ON T_FAC.COMPANY_ID = T_CAR_INFOR.COMPANY_ID)\n" + //
                             "    inner join `options` AS T_OPTS\n" + //
                             "    ON T_CAR_INFOR.CAR_INFOR_ID = T_OPTS.CAR_INFOR_ID\n" + //
-                            "    GROUP BY T_FAC.COMPANY_ID, T_CAR_INFOR.CAR_INFOR_ID";
+                            "GROUP BY T_FAC.COMPANY_ID, T_CAR_INFOR.CAR_INFOR_ID";
                     ResultSet resultSet = statement.executeQuery(query) ;
                     while (resultSet.next()) {
-                        System.out.println(resultSet.getString("COMPANY") + "," +
-                        resultSet.getString("CAR_NAME") + "," +
-                        resultSet.getInt("CNT"));
+                        System.out.println(resultSet.getString("COMPANY") + ", " +
+                            resultSet.getString("CAR_NAME") + ", " +
+                            resultSet.getInt("CNT"));
                     }
                 } else {
-                    System.out.println("----- 작업 키 확인 : 입력한 값 : "+workkey) ;
+                    System.out.println("--- 작업 키 확인 : 입력한 값 : " + workKey);
                 }
             }
-            System.out.println("----- 작업 종료 -----");
+            System.out.println("----- 작업종료 ------");
         } catch (Exception e) {
             // TODO: handle exception
             System.out.println(e.getMessage());
