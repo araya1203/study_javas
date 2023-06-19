624c77f390eea81079dcee1b79ddb66d87b00996
ARAJO <dddd2511u@gmail.com>
2023-06-19 16:37:56 +0900

    first commit

--- src/Inheritances.java
+++ src/Inheritances.java
@@ -1,6 +1,8 @@
 import inheritances.ObjectInheritances;
 import vehicles.Bicycle;
+import vehicles.Car;
 import vehicles.MountainBike;
+import vehicles.RoadCycle;
 
 public class Inheritances {
     public static void main(String[] args) {
@@ -12,9 +14,23 @@ public class Inheritances {
             // Bicycle bicycle = new Bicycle(5500, "blue"); //만들어준 파라미터를 바로 사용하고 싶을때의 인스턴스화
             //System.out.println(bicycle.toString()); //toString을 활용하여 값이 제대로 들어갔는지 확인
             
-            MountainBike mountainBike = new MountainBike();
-            System.out.println();
+            // 상속해 활용
+            //MountainBike mountainBike = new MountainBike();
             
+            //int price;
+            //String color;
+            //int seatHight;
+            //MountainBike mountainBike = new MountainBike(6200, "yellow", 5);
+            
+           // RoadCycle roadcycle = new RoadCycle(200000, "Blue", "Super-Fast Bike");
+           // System.out.println(roadcycle.toString());
+            
+           
+           Car car = new Car();
+           car.speedUp(1);
+           car.speedDown(2);
+           System.out.println();
+
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
---
+++ src/vehicles/RoadCycle.java
@@ -0,0 +1,21 @@
+package vehicles;
+
+public class RoadCycle extends Bicycle {
+    String Name = "";
+    public RoadCycle() {
+
+    }
+    public RoadCycle(int price, String color, String name) {
+        this.Name = name;
+        this.price = price;
+        this.color = color;
+        
+    
+    }
+
+    public String toString() {
+        String str = "name: " + this.Name + ", price: " + this.price + ", color: " + this.color;
+        return str;
+    }
+    
+}
